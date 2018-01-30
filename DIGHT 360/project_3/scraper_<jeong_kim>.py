48637d9d663e342d90a2e2ee92c2d0962bf667c2
Jeongwoo Kim <tjtekrkchlrh@gmail.com>
2018-01-30 00:09:07 -0700

    add DIGHT\ 360

--- DIGHT 360/project_3/scraper_<jeong_kim>.py
+++ DIGHT 360/project_3/scraper_<jeong_kim>.py
@@ -14,11 +14,12 @@ wiki_korea_url = 'https://en.wikipedia.org/wiki/Korea'
 headers = {'user-agent': 'Jeong Kim (tjtekrkchlrh@gmail.com)'}
 response = r.get(wiki_korea_url, headers=headers)
 
-path = '/Users/jeong-ugim/Documents/BYU/2018 Winter Semester/DIGHT 360/project_3/wiki_korea.html'
+path = '/Users/jeong-ugim/Documents/BYU/\
+2018 Winter Semester/DIGHT 360/DIGHT 360/\
+project_3/wiki_korea.html'
 
-file = open(path,"w",encoding='utf8')
+file = open(path, "w", encoding='utf8')
 print(response.text)
 
 file.write(response.text)
-# print(response.text)
 file.close()
